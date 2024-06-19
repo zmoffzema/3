eb60803ded5389df24af12d8ec64dbbf8c956fc4
z_moff <zmoffzema@gmail.com>
2024-06-19 23:51:01 +0300

    Добавьте файлы проекта.

---
+++ практическая 3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string input = "Hello, world!";
+        int length = GetStringLength(input);
+        Console.WriteLine("The length of the string is: " + length);
+    }
+
+    public static int GetStringLength(string str)
+    {
+        return str.Length;
+    }
+}
